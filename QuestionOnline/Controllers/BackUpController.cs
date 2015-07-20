e3021393e2d1a063edf4e8e6773222e796b75a16
mikecdd <523230173@qq.com>
2015-07-20 14:40:19 +0800

    report样稿pk

--- QuestionOnline/Controllers/BackUpController.cs
+++ QuestionOnline/Controllers/BackUpController.cs
@@ -12,22 +12,22 @@ namespace QuestionOnline.Controllers
     public class BackUpController : baseController
     {
         ImproveReportServer ir = new ImproveReportServer();
+        TypeServer ts = new TypeServer();
+        //public ImproveReport AddReport(string typeid,string title,string content,string filename) 
+        //{
+        //    //验证数据
 
-        public ImproveReport AddReport(string typeid,string title,string content,string filename) 
-        {
-            //验证数据
-
-            var model = new ImproveReport() 
-            {
-                regdate=DateTime.Now,
-                regman=Common.CommonClass1.GetUserName(),
-                regmanid=Common.CommonClass1.GetPartyIdCount(),
-                reportcontent=content,
-                title=title,
-                typeid=Convert.ToInt32(typeid)
-            };
-            return ir.Add(model);
-        }
+        //    var model = new ImproveReport() 
+        //    {
+        //        regdate=DateTime.Now,
+        //        regman=Common.CommonClass1.GetUserName(),
+        //        regmanid=Common.CommonClass1.GetPartyIdCount(),
+        //        reportcontent=content,
+        //        title=title,
+        //        typeid=Convert.ToInt32(typeid)
+        //    };
+        //    return ir.Add(model);
+        //}
 
 
 
@@ -35,9 +35,26 @@ namespace QuestionOnline.Controllers
         {
             return View();
         }
-        public ActionResult report()
+        public ActionResult report(int month)
         {
-            return View();
+            var list = ir.FindModelList(o => o.regdate.Value.Month == month).ToList();
+            ViewBag.ReportList = list;
+            ViewBag.Types = ts.FindModelList();
+            return View(list);
+        }
+        /// <summary>
+        ///添加
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [ValidateInput(false)]
+        public ActionResult AddReport(ImproveReport model)
+        {
+            model.regman = Common.CommonClass1.GetUserName();
+            model.regmanid = Common.CommonClass1.GetPartyIdCount();
+            model.regdate = DateTime.Now;
+            return Json(ir.Add(model),JsonRequestBehavior.AllowGet);
+           
         }
     }
 }
